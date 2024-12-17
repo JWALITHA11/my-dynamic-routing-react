d118107a5279ab1425283f0edba187ac93e191ee
JWALITHA11 <jwalithamarella@gmail.com>
2024-12-17 20:43:04 +0530

    changes added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,10 @@
 function Home() {
     return (
     <div className="services-container">
-        <h1> Welcome to Home Page !!</h1>
+        <h1> Welcome to Home Page !!!!</h1>
     
     </div>);
   }
 
-  function myfunction(){
-    return <p>u clicked me</p>
-  }
   
   export default Home;
